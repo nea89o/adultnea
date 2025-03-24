29911e1bfe040f0d3b187a22aae8ec5ed551040a
Linnea Gräf <nea@nea.moe>
2025-03-24 16:42:56 +0100

    feat: Add module system

--- adultnea/client.py
+++ adultnea/client.py
@@ -1,11 +1,62 @@
+import logging
+from pathlib import Path
+from typing import Any, Optional
+
 import discord.ext.commands as commands
+from discord.ext.commands import errors
+import discord
+
+class Context(commands.Context['AdultClient']):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.followup_message: Optional[discord.message.Message] = None
+
+    async def followup(self, content: str):
+        if self.followup_message:
+            await self.followup_message.edit(content)
+        else:
+            self.followup_message = await self.reply(content)
 
 
 class AdultClient(commands.Bot):
     async def on_ready(self):
-        print('Logged in as', self.user)
+        logging.info(f'Logged in as {self.user}')
 
-client = AdultClient(
-    command_prefix='~',
-    self_bot=True,
-)
+    async def setup_hook(self) -> None:
+        await self.reload_all()
+
+    async def reload_all(self):
+        base_path = Path(__file__).parent / 'modules'
+        base_package = __package__ + '.modules'
+        for file in base_path.rglob('*.py'):
+            file: Path
+            relative_file = file.with_name(file.name[:-len('.py')]) \
+                .relative_to(base_path)
+            name = base_package + '.' + str(relative_file).replace('/', '').replace('\\', '.')
+            if name in self.extensions:
+                logging.info(f'Reloading {name}.')
+                await self.reload_extension(name)
+            else:
+                logging.info(f'Loading extension from {file} ({relative_file}) as {name}')
+                await self.load_extension(name)
+
+    def __init__(self):
+        super().__init__(
+            command_prefix='~',
+            self_bot=True,
+            chunk_guilds_at_startup=False,
+        )
+
+    async def on_command_error(
+            self, context: Context,
+            exception: errors.CommandError, /) -> None:
+        logging.error(
+            f"Encountered error in {context.command.qualified_name if context.command else 'unknown'}:",
+            exc_info=exception)
+
+    async def get_context(
+            self, *args, **kwargs) -> Any:
+        return await super().get_context(*args, **kwargs, cls=Context)
+
+
+client = AdultClient()
---
+++ adultnea/modules/meta.py
@@ -0,0 +1,31 @@
+import logging
+from tarfile import TruncatedHeaderError
+
+import discord.ext.commands as commands
+
+from adultnea.client import AdultClient, client, Context
+
+
+@commands.group(
+    invoke_without_command=True
+)
+async def mod(ctx: Context):
+    pass
+
+
+@mod.command()
+async def ls(ctx: Context):
+    module_names = [it.replace(__package__, '') for it in ctx.bot.extensions.keys()]
+    await ctx.followup('Modules:\n' + '\n'.join(f'- `{name}`' for name in module_names))
+
+
+@mod.command()
+async def reload(ctx: Context):
+    await ctx.followup(content='Reload started.')
+    await ctx.bot.reload_all()
+    await ctx.followup(content='Reloaded.')
+
+
+async def setup(bot: AdultClient):
+    logging.info('Loading meta module.')
+    bot.add_command(mod)
